c5d165792fd67402676465b045abd06c518b4cef
DarkoKrezic <darkotronic@gmail.com>
2023-10-18 16:30:28 +0200

    some  styling and  secondpage for  chat

--- app/page.tsx
+++ app/page.tsx
@@ -4,50 +4,55 @@ import DropdownComponent from "@/components/dropDownComponent";
 import ChatComponent from "@/components/chatComponent";
 
 export default function Home() {
+  const [page, setPage] = useState<string | null>("home");
   const [selected, setSelected] = useState<string | null>(null);
-  const [topic, setTopic] = useState("");
+
   const backgroundImage = "url('/kvisko.jpeg')";
 
   const handleSelected = (value: string) => {
     setSelected(value);
-    setTopic(value);
+    setPage("chat");
   };
 
   return (
     <div>
-      <div
-        style={{
-          backgroundImage: backgroundImage,
-          backgroundSize: "cover", // make it cover the whole div
-          position: "absolute", // make it fill the whole screen
-          width: "100%",
-          height: "100%",
-          zIndex: "-1",
-          backgroundPosition: "center",
-        }}
-      ></div>
-      <h2
-        className="text-4xl font-bold text-center py-10"
-        style={{
-          backgroundColor: "rgba(0, 0, 0, 0.49)",
-          borderRadius: "25px",
-          textShadow: "3px 5px 10px rgb(8, 8, 9)",
-        }}
-      >
-        Hi, ich bin Quizzko!
-      </h2>
-      <p
-        className="text-center text-2xl px-2 py-4"
-        style={{
-          backgroundColor: "rgba(0, 0, 0, 0.49)",
-          borderRadius: "25px",
-        }}
-      >
-        Bitte wähle ein Themengebiet aus der Liste:
-      </p>
-
-      <DropdownComponent onSetSelected={handleSelected} />
-      {selected && <ChatComponent selectedOption={selected} />}
+      {/* Hier auf der Grundlage der 'page'-Variable zu wechseln. */}
+      {page === "home" && (
+        <>
+          <div
+            style={{
+              backgroundImage: backgroundImage,
+              backgroundSize: "cover",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              zIndex: "-1",
+              backgroundPosition: "center",
+            }}
+          ></div>
+          <h2
+            className="text-6xl font-bold text-center py-10"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.49)",
+              textShadow: "3px 5px 10px rgb(8, 8, 9)",
+            }}
+          >
+            Hi, ich bin Quizzko!
+          </h2>
+          <p
+            className="text-center text-2xl px-2 py-4"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.49)",
+            }}
+          >
+            Bitte wähle ein Themengebiet aus der Liste:
+          </p>
+          <DropdownComponent onSetSelected={handleSelected} />
+        </>
+      )}
+      {page === "chat" && selected && (
+        <ChatComponent selectedOption={selected} />
+      )}
     </div>
   );
 }
